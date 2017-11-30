e4e3e0801f1e2d30bff7a206a5278e44931aac0c
Ramya Karri <rkarri@shaklee.com>
2017-11-30 10:09:45 -0800

    updating userDataStorageDao to use properties file

--- src/main/java/com/shaklee/DAO/UserDataStorageDAO.java
+++ src/main/java/com/shaklee/DAO/UserDataStorageDAO.java
@@ -3,25 +3,22 @@ package com.shaklee.DAO;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@PropertySource(value = "classpath:UserDataStorage.properties")
 public class UserDataStorageDAO {
 	
 	@Autowired
     JdbcTemplate jdbcTemplate;
 	
    
-	/*@Autowired
-    PropertiesObject props;*/
-
-	
-	private String GET_QUESTIONS = "SELECT S.USER_ID, S.EMAIL, S.FIRST_NAME AS s_first_name, S.LAST_NAME  AS s_last_name, S.EMAIL AS s_email, S.OPT_IN, " + 
-			"S.ANSWERS_JSON, S.HEALTH_PROFILE_ID " + 
-			"FROM HQ_USER_DATA_STORAGE S WHERE ??=? "; 
-	
+	@Value("${GET_QUESTIONS}")
+	private String GET_QUESTIONS;
 
 	public List getQuestions(final String healthProfileId) {
 
