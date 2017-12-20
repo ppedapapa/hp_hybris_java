cb290d19aba2c588c661c1e26c6f8c86b10eeef0
Ramya Karri <rkarri@shaklee.com>
2017-12-20 13:58:31 -0800

    commit before testing new files

--- src/main/java/com/shaklee/Application.java
+++ src/main/java/com/shaklee/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -57,6 +58,8 @@ public class Application {
         		password("us@dev").build()
         		;
     }
+    
+ 
     
 
 }
