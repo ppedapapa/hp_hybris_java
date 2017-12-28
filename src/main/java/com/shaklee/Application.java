106d527323398887748faa2e4aa13090372b792c
Ramya Karri <rkarri@shaklee.com>
2017-12-28 10:37:50 -0800

    working both spring boot and jetty

--- src/main/java/com/shaklee/Application.java
+++ src/main/java/com/shaklee/Application.java
@@ -8,8 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -17,11 +19,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	    @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(Application.class);
+	    }
+
+	    public static void main(String[] args) throws Exception {
+	        SpringApplication.run(Application.class, args);
+	    }
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
--- src/main/java/com/shaklee/resources/HealthQuestionaireResource.java
+++ src/main/java/com/shaklee/resources/HealthQuestionaireResource.java
@@ -1,6 +1,6 @@
 package com.shaklee.resources;
 
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import java.util.List;
 
@@ -8,22 +8,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shaklee.DAO.UserDataStorageDAO;
 
-@RestController
+@Controller
 public class HealthQuestionaireResource {
 	
 	@Autowired 
 	UserDataStorageDAO userDataStorageDAO;
 	
-	@RequestMapping(path = "/get", method = POST)
-	public ResponseEntity<Object> get(@RequestBody UserRequest request)
+	@RequestMapping(path = "/get", method = GET)
+	public String get()
 	{
-		List data = userDataStorageDAO.getQuestions(request.health_profile_id);
+		return "redirect:/getByHp";
+		/*List data = userDataStorageDAO.getQuestions(request.health_profile_id);
+		
+		return new ResponseEntity<Object>( new QuestionsResponse("Got the data" + data.size()), HttpStatus.OK);
+		*/
+	}
+
+	@RequestMapping(path = "/getByHp", method = GET)
+	public ResponseEntity<Object> getByHp()
+	{
+		List data = userDataStorageDAO.getQuestions("a5he2ptf2bjnspk0444exjv76");
 		
 		return new ResponseEntity<Object>( new QuestionsResponse("Got the data" + data.size()), HttpStatus.OK);
 	}
