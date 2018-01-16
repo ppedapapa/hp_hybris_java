cd7020f2288d73349dc810c72598cc5e0f68f071
Chitaure, Dil <dchitaure@shaklee.com>
2018-01-16 12:12:54 -0800

    ssl fix

--- angular-frontend/src/app/app.service.ts
+++ angular-frontend/src/app/app.service.ts
@@ -15,7 +15,23 @@ export class AppService {
     headers.append('Content-Type', 'application/json; charset=utf-8');
 
         return this.http.get('https://localhost:9002/shakleeintegration/v2/shakleeUS/products/89384?fields=DEFAULT',
-{headers: headers})
+      {headers: headers})
+     .subscribe(
+         data => {
+             console.log(data);
+             return data;
+         }
+     );
+    }
+
+  isUserLogin() {
+      const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json; charset=utf-8');
+    headers.append('Access-Control-Allow-Credentials', 'true');
+    headers.append('Access-Control-Allow-Origin', '*');
+     headers.append('Cache-Control', 'no-cache');
+      return this.http.get('public/hp/testUserId',
+      {headers: headers})
      .subscribe(
          data => {
              console.log(data);
