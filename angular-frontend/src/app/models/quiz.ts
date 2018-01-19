6dda74edcfa03a3e48d82c1b6e31ab656c55c0bb
spaipalle <spaipalle@shaklee.com>
2018-01-19 11:26:16 -0800

    Add Header and Footer

--- angular-frontend/src/app/models/quiz.ts
+++ angular-frontend/src/app/models/quiz.ts
@@ -4,15 +4,15 @@ import { Page } from './page';
 
 export class Quiz {
     public id: number;
-    public name: string;
+    public label: string;
     public description: string;
     public config: QuestionConfig;
     public questions: Question[];
     public pages: Page[];
 
-    constructor(id: number, name: string, description: string, config: QuestionConfig, questions: Question[], pages: Page[]) {
+    constructor(id: number, label: string, description: string, config: QuestionConfig, questions: Question[], pages: Page[]) {
         this.id = id;
-        this.name = name;
+        this.label = label;
         this.description = description;
         this.config = config;
         this.questions = questions;
