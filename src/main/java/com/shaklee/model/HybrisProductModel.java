3f14f1f83f18e3299d1e1f1b87a84f4f8bb8d785
Ramya Karri <rkarri@shaklee.com>
2018-01-22 14:41:05 -0800

    updating getProducts API and also adding ssl true for shakleedev.com

--- src/main/java/com/shaklee/model/HybrisProductModel.java
+++ src/main/java/com/shaklee/model/HybrisProductModel.java
@@ -13,35 +13,30 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class HybrisProductModel {
-	
-	public void getProducts()
-	{
-	    final String uri = "https://www.shakleedev.com:9002/shakleeintegration/v2/shakleeUS/products/89384?fields=DEFAULT";
 
-	    URL url;
-		try {
-			url = new URL(uri);
+	static {
+		javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(new javax.net.ssl.HostnameVerifier() {
+			public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
+				if (hostname.endsWith("shakleedev.com")) {
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+
+	public void getProducts() {
+		final String uri = "https://www.shakleedev.com:9002/shakleeintegration/v2/shakleeUS/products/89384?fields=DEFAULT";
 		
-	    HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
-	    
-	    con.setRequestMethod("GET");
-	    con.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-	    
-		con.setRequestProperty("Access-Control-Allow-Credentials", "true");
-		con.setRequestProperty("Access-Control-Allow-Origin", "*");
- 
-		System.out.println("\nSending request to URL : " + url);
-		System.out.println("Response Code : " + con.getResponseCode());
-		System.out.println("Response Message : " + con.getResponseMessage());
+		URL url;
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+	        String quote = restTemplate.getForObject(uri, String.class);
+	        System.out.println(quote.toString());
 
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		
 		}
-
-	   // ResponseEntity<String> result = con..getForEntity(uri, String.class);
-
-	    // System.out.println(result);
 	}
 
 }
