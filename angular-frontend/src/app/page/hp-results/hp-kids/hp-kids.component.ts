b0f057ab62bd254b3f3449191cdfbcc257fe0ec6
Chitaure, Dil <dchitaure@shaklee.com>
2018-03-06 11:26:02 -0800

    Fix multiple healthprint reports

--- angular-frontend/src/app/page/hp-results/hp-kids/hp-kids.component.ts
+++ angular-frontend/src/app/page/hp-results/hp-kids/hp-kids.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HpConfigService } from "../../../services/hp-config.service";
 import { HealthPrintResultsService } from "../../../services/hp-results.service";
 
 @Component({
@@ -10,13 +11,21 @@ export class HpKidsComponent implements OnInit {
   showKids = false;
   bundles = [];
   kidsBundle = [];
-  kids = this.healthPrintResultsService.isKids();
+  kids = false;
 
-  constructor(private healthPrintResultsService: HealthPrintResultsService) { }
+  constructor(private healthPrintResultsService: HealthPrintResultsService,
+              private hpConfigService: HpConfigService ) { }
 
   ngOnInit() {
     const tiers: string[] = ["KIDS"];
+  
+      this.healthPrintResultsService.healthPrintResultInfo.subscribe( healthPrintResultInfo => {
+          this.kids = (healthPrintResultInfo['questions']['age'] <= this.hpConfigService.getKosherSkus())?true:false;
+      });
+  
     this.healthPrintResultsService.getBundles().subscribe(bundle => {
+      this.bundles = [];
+      this.kidsBundle = [];
       for(let val in bundle) {
           if(tiers.indexOf(bundle[val]['bundle']) !== -1) {
               this.bundles.push(bundle[val]);
