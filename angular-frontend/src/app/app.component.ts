1b2b94ab398ab9b7050c40f1369a5f33c4918ae0
spaipalle <spaipalle@shaklee.com>
2018-03-08 14:36:04 -0800

    Add Spinner

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { QuestionsService } from './services/questions.service';
-import { AppService } from './app.service';
+// import as RouterEvent to avoid confusion with the DOM Event
+import { Event as RouterEvent,     Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +10,24 @@ import { AppService } from './app.service';
 })
 export class AppComponent implements OnInit {
 
+  loading = true;
   constructor(private translate: TranslateService,
-              private serverService: AppService) {
+              private router: Router) {
+    router.events.subscribe((event: RouterEvent) => {
+        this.navigationInterceptor(event)
+    });
     translate.setDefaultLang('us-en');
   }
 
-  ngOnInit() {
-    // this.serverService.isUserLogin();
-  }
+  ngOnInit() {}
 
+  // Shows and hides the loading spinner during RouterEvent changes
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+        this.loading = true
+    }
+    if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+        this.loading = false
+    }
+  }
 }
