790e569a95279881d81323e1b62dbf7b1a2eb093
Chitaure, Dil <dchitaure@shaklee.com>
2018-03-12 15:56:11 -0700

    add appservice

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Event as RouterEvent,     Router, NavigationStart, NavigationEnd, Navig
 
 import { environment } from '../environments/environment';
 import { GoogleAnalyticsService } from "./services/google-analytics.service";
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,7 @@ export class AppComponent implements OnInit {
 
   loading = true;
   constructor(private googleAnalyticsService: GoogleAnalyticsService,
+  			  private appService: AppService,
               private translate: TranslateService,
               private router: Router) {
     router.events.subscribe((event: RouterEvent) => {
